2c390875c974f589896deebfeb5e4bbed3ac4128
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-13 17:40:11 -0400

    Added basics on the client

---
+++ SimplePassive.Client/Passive.cs
@@ -0,0 +1,26 @@
+﻿using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
+
+namespace SimplePassive.Client
+{
+    /// <summary>
+    /// Client script that does the real hard work.
+    /// </summary>
+    public class Passive : BaseScript
+    {
+        #region Fields
+
+        /// <summary>
+        /// The activation of passive mode for specific players.
+        /// </summary>
+        public readonly Dictionary<string, bool> activations = new Dictionary<string, bool>();
+        /// <summary>
+        /// If the local player has passive enabled or disabled.
+        /// </summary>
+        public bool localActivation = Convert.ToBoolean(API.GetConvarInt("simplepassive_default", 0));
+
+        #endregion
+    }
+}
