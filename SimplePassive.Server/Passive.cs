e2494a4820ac67f50279c9f9d4aa133caa2daca5
Hannele Ruiz <justlemoncl@gmail.com>
2020-04-12 22:38:00 -0400

    Added basic script with a dictionary for the activation of each player

---
+++ SimplePassive.Server/Passive.cs
@@ -0,0 +1,20 @@
+﻿using CitizenFX.Core;
+using System.Collections.Generic;
+
+namespace SimplePassive.Server
+{
+    /// <summary>
+    /// Script that handle the Passive Mode between the clients.
+    /// </summary>
+    public class Passive : BaseScript
+    {
+        #region Fields
+
+        /// <summary>
+        /// The activation of passive mode for specific players.
+        /// </summary>
+        public readonly Dictionary<string, bool> activations = new Dictionary<string, bool>();
+
+        #endregion
+    }
+}
