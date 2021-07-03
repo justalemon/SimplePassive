ea2a6fc6dd8856887342b37dfdd6a32034cc1c3a
QuadrupleTurbo <46331538+QuadrupleTurbo@users.noreply.github.com>
2021-07-03 14:15:28 +0100

    Removal of camera collisions between other players

--- SimplePassive.Client/Passive.cs
+++ SimplePassive.Client/Passive.cs
@@ -191,6 +191,8 @@ namespace SimplePassive.Client
                     otherPed.DisableCollisionsThisFrame(localVehicle, printNextTick);
                     // Other Player vs Local Hooked (if present)
                     otherPed.DisableCollisionsThisFrame(localHooked, printNextTick);
+                    // Disable cam collision for other ped
+                    API.DisableCamCollisionForEntity(otherPed.Handle);
 
                     // Other Vehicle vs Local Player
                     otherVehicle?.DisableCollisionsThisFrame(localPed, printNextTick);
@@ -198,6 +200,8 @@ namespace SimplePassive.Client
                     otherVehicle?.DisableCollisionsThisFrame(localVehicle, printNextTick);
                     // Other Vehicle vs Local Hooked (if present)
                     otherVehicle?.DisableCollisionsThisFrame(localHooked, printNextTick);
+                    // Disable cam collision for other vehicle
+                    API.DisableCamCollisionForEntity(otherVehicle.Handle);
 
                     // Other Hooked vs Local Player
                     otherHooked?.DisableCollisionsThisFrame(localPed, printNextTick);
@@ -205,6 +209,8 @@ namespace SimplePassive.Client
                     otherHooked?.DisableCollisionsThisFrame(localVehicle, printNextTick);
                     // Other Hooked vs Local Hooked (if present)
                     otherHooked?.DisableCollisionsThisFrame(localHooked, printNextTick);
+                    // Disable cam collision for other vehicle trailer if hooked
+                    API.DisableCamCollisionForEntity(otherHooked.Handle);
 
                     // On debug mode, draw markers over the other player entities (if found)
                     if (Convars.Debug)
