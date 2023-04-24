9258829bf10f37a6fe6a22ede619cf870fefc2a9
Hannele Ruiz <justlemoncl@gmail.com>
2023-04-24 05:34:04 -0400

    Added Lua function to print debug messages

--- server.lua
+++ server.lua
@@ -3,6 +3,14 @@ Activations = {}
 -- The activations that override the dictionary above.
 Overrides = {}
 
+function Debug(message)
+    if GetConvarInt("simplepassive_debug", 0) == 0 then
+        return
+    end
+
+    print(message)
+end
+
 function GetDefaultActivation()
     return GetConvarInt("simplepassive_default", 0) ~= 0
 end
